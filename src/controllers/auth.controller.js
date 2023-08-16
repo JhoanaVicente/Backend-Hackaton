0dea21a6f7291214f6ca3ab5a120bab552b9cd78
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-08-16 14:41:16 +0200

    validando login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -14,20 +14,50 @@ export const register = async (req, res) => {
         password: passwordHash,
     });
 
-    const userSaved = await newUser.save();
-    const token = await createAccessToken({id: userSaved._id});
+    const userFound = await newUser.save();
+    const token = await createAccessToken({id: userFound._id});
 
     res.cookie('token', token);
     res.json({
-        id: userSaved._id,
-        username: userSaved.username,
-        email: userSaved.email,
-        createdAt: userSaved.createdAt,
-        updatedAt: userSaved.createdAt
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.createdAt
     });
    } catch (error) {
     res.status(500).json({ message: error.message });
    }    
 };
 
-export const login = (req, res) => res.send("login");
+export const login = async (req, res) => {
+    const {email, password } = req.body;
+
+   try {
+    const userFound = await User.findOne({email})
+    if (!userFound) return res.status(400).json({message: "User not found"});
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch) return res.status(400).json ({ message: "Incorrect password" });
+
+    const token = await createAccessToken({id: userFound._id});
+
+    res.cookie('token', token);
+    res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.createdAt
+    });
+   } catch (error) {
+    res.status(500).json({ message: error.message });
+   }    
+};
+
+export const logout = (req, res) => {
+    res.cookie('token', "", {
+        expires: new Date(0)
+    })
+    return res.sendStatus(200);
+};
