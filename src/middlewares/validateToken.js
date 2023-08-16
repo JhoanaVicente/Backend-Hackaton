196cbb77a6f911c4abcd671b470b007c9feb0321
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-08-16 19:04:23 +0200

    task CRUD

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,7 +4,7 @@ export const authRequired = (req, res, next) => {
     const { token } = req.cookies;
 
     if (!token)
-        return res.status(401).jso({ message: "No token, authorization denied"});
+        return res.status(401).json({ message: "No token, authorization denied"});
         
         jwt.verify(token, TOKEN_SECRET, (err, user) => {
             if (err) return res.status(403).json({ message: "Invalid token"});
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,8 +1,23 @@
 import {Router} from 'express'
 import { authRequired } from '../middlewares/validateToken.js'
+import {
+    getTask,
+    getTasks,
+    createTask,
+    updateTask,
+    deleteTask,
+} from '../controllers/tasks.controller.js';
 
 const router = Router()
 
-router.get('/tasks', authRequired, (req, res) => res.send('tasks'))
+router.get('/tasks', authRequired, getTasks);
+
+router.get('/tasks/:id', authRequired, getTask);
+
+router.post('/tasks', authRequired, createTask);
+
+router.delete('/tasks/:id', authRequired, deleteTask);
+
+router.put('/tasks/:id', authRequired, updateTask);
 
 export default router
