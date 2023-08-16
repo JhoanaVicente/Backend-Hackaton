7524612573b7c70f933c9873ef341d91a7d2a8a7
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-08-16 13:23:27 +0200

    instalando bcrypt para los tokens

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,17 @@
-import User from '../models/user.model.js'
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs'
 
 export const register = async (req, res) => {
     const {email, password, username} = req.body
 
    try {
+
+    const passwordHash = await bcrypt.hash(password, 10)
+
     const newUser = new User({
         username,
         email,
-        password,
+        password: passwordHash,
     })
     
     const userSave = await newUser.save();
