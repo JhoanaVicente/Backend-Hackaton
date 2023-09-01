bc323736eb64d712bbd8703185700429210ee6de
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-09-01 20:55:58 +0200

    Frontend configurado

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,6 +1,18 @@
+import {BrowserRouter, Routes, Route } from "react-router-dom"
+
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home page</h1>} />
+        <Route path='/login' element={<h1>Login</h1>} />
+        <Route path='/register' element={<h1>Register</h1>} />
+        <Route path='/tasks' element={<h1>Tasks page</h1>} />
+        <Route path='/add-task' element={<h1>new task</h1>} />
+        <Route path='/tasks/:id' element={<h1>update task</h1>} />
+        <Route path='/profile' element={<h1>profile</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
