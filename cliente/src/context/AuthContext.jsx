74070418ff7a782a32d9eaf3cc3e2b118a0ab0dd
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-09-03 16:57:41 +0200

    Frontend:Vigilando errores

--- cliente/src/context/AuthContext.jsx
+++ cliente/src/context/AuthContext.jsx
@@ -5,16 +5,17 @@ import { set } from "mongoose";
 export const AuthContext = createContext();
 
 export const useAuth = () => {
-    const context = useContext(AuthContext)
+    const context = useContext(AuthContext);
     if (!context) {
         throw new Error("useAuth must be used within an AuthProvider");
     }
     return context;
-}
+};
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [errors, setErrors] = useState([]);
 
     const signup = async (user) => {
         try{
@@ -23,7 +24,8 @@ export const AuthProvider = ({ children }) => {
             setUser(res.data);
             setIsAuthenticated(true);
         } catch (error) {
-            console.log(error);
+            console.log(error.response)
+            setErrors(error.response.data);
         }
     };
 
@@ -33,6 +35,7 @@ export const AuthProvider = ({ children }) => {
         signup,
         user,
         isAuthenticated,
+        errors
      }}
      >
         {children}
--- cliente/src/pages/RegisterPage.jsx
+++ cliente/src/pages/RegisterPage.jsx
@@ -9,7 +9,7 @@ function RegisterPage() {
             handleSubmit,
             formState: {errors},
         } = useForm();
-        const {signup, isAuthenticated} = useAuth();
+        const {signup, isAuthenticated, errors: registerErrors } = useAuth();
         const navigate = useNavigate();
 
         useEffect(() => {
@@ -21,7 +21,14 @@ function RegisterPage() {
         });
 
     return (
-        <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
+        <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+            {
+             registerErrors.map((error, i) => (
+                <div className="bg-red-500 p-2 text-white" key={i}>
+                    {error}
+                </div>
+             ))
+            }
             <form onSubmit={onSubmit}>
                 <input type="text" {...register('username', {required: true})}
                     className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
