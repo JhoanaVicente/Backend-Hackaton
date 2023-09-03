4e1c7bfcc8cc4428f4d3821033e92b5e0056de82
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-09-03 16:58:50 +0200

    Backend:Ayudando a frontend con los errores en middlewares y controllers

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,7 +8,7 @@ export const register = async (req, res) => {
    try {
     const userFound = await User.findOne({email});
     if (userFound)
-        return res.status(400).json({ message: ["The email is already in use"] });
+        return res.status(400).json(["The email is already in use"]);
 
     const passwordHash = await bcrypt.hash(password, 10)
 
@@ -18,16 +18,16 @@ export const register = async (req, res) => {
         password: passwordHash,
     });
 
-    const userFound = await newUser.save();
+    const userSaved = await newUser.save();
     const token = await createAccessToken({id: userFound._id});
 
     res.cookie('token', token);
     res.json({
-        id: userFound._id,
-        username: userFound.username,
-        email: userFound.email,
-        createdAt: userFound.createdAt,
-        updatedAt: userFound.createdAt
+        id: userSaved._id,
+        username: userSaved.username,
+        email: userSaved.email,
+        createdAt: userSaved.createdAt,
+        updatedAt: userSaved.createdAt
     });
    } catch (error) {
     res.status(500).json({ message: error.message });
