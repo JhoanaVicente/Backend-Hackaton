10278808e65913757d5852d48a683a596f7fa7e5
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-09-25 20:58:42 +0200

    modifying schemas file

--- src/schemas/auth.shema.js
+++ src/schemas/auth.shema.js
@@ -2,19 +2,19 @@ import {z} from 'zod';
 
 export const resgisterSchema = z.object({
     username: z.string ({
-        required_error: 'Username is required'
+        required_error: 'Username is required',
     }),
     email: z.
     string({
         required_error: 'Email is required'
     }).email({
-        message: 'Invalid email'
+        message: 'Email is not valid',
     }),
     password: z.
     string({
-        required_error: 'Password is required'
+        required_error: 'Password is required',
     }).min(6, {
-        message: 'Password must be at least 6 characters'
+        message: 'Password must be at least 6 characters',
     }),
 });
 
@@ -24,7 +24,7 @@ export const loginSchema = z.object({
             required_error: 'Email is required',
         })
         .email({
-            message: 'Invalid email',
+            message: 'Email is not valid',
         }),
     password: z
         .string({
